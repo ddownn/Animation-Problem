182aab209d026eef748c3c1ea3980dfdf2010363
Paul <ddownn@gmail.com>
2020-12-09 17:48:40 -0500

    Initial Commit

---
+++ Animation Problem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation Problem
+//
+//  Created by Paul Bryan on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
